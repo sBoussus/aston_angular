863615b460651f5136a9bd3436d9a56b056c49bf
tpx_samuel <samuel@toutenpixel.fr>
2020-11-02 14:08:31 +0100

    avant exercices

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent {
+
+  title: string = 'header component';
+  altText: string = 'photo d\'un loup';
+  srcPath: string = 'https://static.nationalgeographic.fr/files/styles/image_3200/public/01_booktalk_wolves_0.jpg?w=1600&h=900';
+  authenticated: boolean = false;
+  options: string[] = ['red', 'blue', 'green'];
+  borderColor: string = 'black';
+
+  constructor() {
+    this.updateUI();
+  }
+
+  updateUI() {
+    setTimeout(
+      () => {
+        console.log('Update UI');
+        this.title += ' updated !';
+        this.authenticated = !this.authenticated;
+        this.options.push('New option');
+      }, 1000
+    );
+  }
+
+  onChange(event: any) {
+    let value: string = event.target.value;
+    console.log(value);
+    if(value === 'New option') {
+      this.borderColor = '#ff6633';
+    } else {
+      this.borderColor = value;
+    }
+  }
+
+}
